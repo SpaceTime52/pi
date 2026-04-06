9a7fa9e6b1f9afe876fe6f77c84079daf33af662
jeonghyeon-net <kim@jeonghyeon.net>
2026-04-06 17:17:35 +0900

    dist 빌드 반영

--- 01_EXTENSIONS/notify/dist/notify.d.ts
+++ 01_EXTENSIONS/notify/dist/notify.d.ts
@@ -1,1 +1,3 @@
-export declare function notify(title: string, body: string): void;
+type Writer = (s: string) => void;
+export declare function notify(title: string, body: string, write?: Writer): void;
+export {};
--- 01_EXTENSIONS/notify/dist/notify.js
+++ 01_EXTENSIONS/notify/dist/notify.js
@@ -1,15 +1,15 @@
-function notifyOSC777(title, body) {
-    process.stdout.write(`\x1b]777;notify;${title};${body}\x07`);
+function notifyOSC777(title, body, write) {
+    write(`\x1b]777;notify;${title};${body}\x07`);
 }
-function notifyOSC99(title, body) {
-    process.stdout.write(`\x1b]99;i=1:d=0;${title}\x1b\\`);
-    process.stdout.write(`\x1b]99;i=1:p=body;${body}\x1b\\`);
+function notifyOSC99(title, body, write) {
+    write(`\x1b]99;i=1:d=0;${title}\x1b\\`);
+    write(`\x1b]99;i=1:p=body;${body}\x1b\\`);
 }
-export function notify(title, body) {
+export function notify(title, body, write = (s) => process.stdout.write(s)) {
     if (process.env.KITTY_WINDOW_ID) {
-        notifyOSC99(title, body);
+        notifyOSC99(title, body, write);
     }
     else {
-        notifyOSC777(title, body);
+        notifyOSC777(title, body, write);
     }
 }
