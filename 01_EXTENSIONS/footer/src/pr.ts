602904111b593cb48df9cccd7f695b8e6e1cda2c
jeonghyeon-net <kim@jeonghyeon.net>
2026-04-22 15:48:20 +0900

    남은 확장과 스킬 정리

--- 01_EXTENSIONS/footer/src/pr.ts
+++
@@ -1,3 +0,0 @@
-export { buildPullRequestStatusEntries } from "./pr-display.js";
-export { getPullRequestStatus } from "./pr-query.js";
-export { normalizeMergeState, normalizePullRequest, normalizeReviewState, samePullRequestStatus } from "./pr-normalize.js";
--- 01_EXTENSIONS/web-access/src/index.ts
+++
@@ -1,8 +0,0 @@
-import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
-import { createWebSearchTool, createCodeSearchTool, createFetchContentTool } from "./tools.js";
-
-export default function (pi: ExtensionAPI) {
-	pi.registerTool(createWebSearchTool());
-	pi.registerTool(createCodeSearchTool());
-	pi.registerTool(createFetchContentTool());
-}
